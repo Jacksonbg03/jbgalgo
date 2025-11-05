e507ccd3055cf46c94ee987d5271cab74266ee15
Jacksonbg03 <jacksonbhaktig03@gmail.com>
2025-11-05 17:17:21 +0700

    drop: unused code

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,10 @@
 import express from "express";
 import {
   getLeaderboard,
-  updateProfile,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/leaderboard", getLeaderboard);
-router.post("/updateProfile", updateProfile);
 
 export default router;
